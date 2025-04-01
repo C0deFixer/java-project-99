8eb2fc22a32adae916e5c2e5e5a137f9f266ebe6
Dmitry Sharypov <Sharypov_D@mail.ru>
2025-04-01 21:05:32 +0300

    thin controller

--- src/main/java/hexlet/code/controller/api/TaskController.java
+++ src/main/java/hexlet/code/controller/api/TaskController.java
@@ -1,19 +1,12 @@
 package hexlet.code.controller.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.dto.TaskCreateDto;
 import hexlet.code.dto.TaskDto;
 import hexlet.code.dto.TaskParamsDto;
 import hexlet.code.dto.TaskUpdateDto;
-import hexlet.code.exception.ResourceNotFoundException;
-import hexlet.code.mapper.TaskMapper;
-import hexlet.code.model.Task;
-import hexlet.code.repository.TaskRepository;
-import hexlet.code.specification.TaskSpecification;
+import hexlet.code.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -36,28 +29,15 @@ import java.util.List;
 public class TaskController {
 
     @Autowired
-    private TaskRepository taskRepository;
-
-    @Autowired
-    private TaskSpecification specBuilder;
-
-    @Autowired
-    private TaskMapper mapper;
-
-    @Autowired
-    private ObjectMapper om;
-
-    private static final int OFF_SET = 10;
+    private TaskService service;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<TaskDto>> index(TaskParamsDto taskParamsDto,
                                                @RequestParam(defaultValue = "1") int page) {
-        var spec = specBuilder.build(taskParamsDto);
-        Page<Task> tasks = taskRepository.findAll(spec, PageRequest.of(page - 1, OFF_SET));
-        var result = tasks.map(mapper::map);
-        List<TaskDto> taskList = result.getContent();
+
+        List<TaskDto> taskList = service.getAll(taskParamsDto, page);
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("X-Total-Count", String.valueOf(taskList.size()))
@@ -67,38 +47,28 @@ public class TaskController {
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public TaskDto show(@PathVariable Long id) {
-        Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found!"));
-        return mapper.map(task);
+        return service.show(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated()")
     public TaskDto create(@Valid @RequestBody TaskCreateDto dto) {
-        Task task = taskRepository.save(mapper.map(dto));
-        TaskDto taskDto = mapper.map(task);
-        return taskDto;
+        return service.create(dto);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("isAuthenticated()")
     public TaskDto update(@Valid @RequestBody TaskUpdateDto dto, @PathVariable Long id) {
-        Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found!"));
-        mapper.update(dto, task);
-        taskRepository.save(task);
-        return mapper.map(task);
+        return service.update(dto, id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("isAuthenticated()")
     public void delete(@PathVariable Long id) {
-        Task task = taskRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found!"));
-        taskRepository.deleteById(id);
+        service.delete(id);
     }
 
 }
---
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -0,0 +1,66 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.TaskCreateDto;
+import hexlet.code.dto.TaskDto;
+import hexlet.code.dto.TaskParamsDto;
+import hexlet.code.dto.TaskUpdateDto;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.TaskMapper;
+import hexlet.code.model.Task;
+import hexlet.code.repository.TaskRepository;
+import hexlet.code.specification.TaskSpecification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository repository;
+
+    @Autowired
+    private TaskMapper mapper;
+
+    @Autowired
+    private TaskSpecification specBuilder;
+
+    private static final int OFF_SET = 10;
+
+    public List<TaskDto> getAll(TaskParamsDto taskParamsDto, int page) {
+        var spec = specBuilder.build(taskParamsDto);
+        Page<Task> tasks = repository.findAll(spec, PageRequest.of(page - 1, OFF_SET));
+        var result = tasks.map(mapper::map);
+        List<TaskDto> taskList = result.getContent();
+        return repository.findAll().stream().map(mapper::map).toList();
+    }
+
+    public TaskDto show(Long id) {
+        Task task = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found!"));
+        return mapper.map(task);
+    }
+
+    public TaskDto create(TaskCreateDto dto) {
+        Task task = mapper.map(dto);
+        repository.save(task);
+        return mapper.map(task);
+    }
+
+    public TaskDto update(TaskUpdateDto dto, Long id) {
+        Task task = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found!"));
+        mapper.update(dto, task);
+        repository.save(task);
+        return mapper.map(task);
+    }
+
+    public void delete(Long id) {
+        Task task = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found!"));
+        repository.deleteById(id);
+    }
+
+}
