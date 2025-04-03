7cf0930d8e5e6f93a20c04d34995cc49d8e663b2
dmitry <Sharypovdmi@gmail.com>
2025-04-03 13:29:55 +0300

    get user by Email is Optional type

--- src/main/java/hexlet/code/repository/UserRepository.java
+++ src/main/java/hexlet/code/repository/UserRepository.java
@@ -4,8 +4,10 @@ import hexlet.code.model.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
